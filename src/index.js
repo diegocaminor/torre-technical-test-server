a9e487b3b578302994ef5805f7b2ae1bb39a71a0
Diego Camino <diego.camino.r@gmail.com>
2020-12-28 02:02:48 -0500

    Add errorHandlers middleware and cors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const logger = require("morgan");
+const cors = require("cors");
 
 // Environment Variables
 const { config } = require("./config/index");
@@ -12,15 +13,23 @@ const { config } = require("./config/index");
 const biosApi = require("./routes/bios");
 const jobsApi = require("./routes/jobs");
 
+// Import errorHandlers middleware
+const { logErrors, errorHandler } = require("./utils/middleware/errorHandlers");
+
 // Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(logger("dev"));
+app.use(cors());
 
 // Routes
 biosApi(app);
 jobsApi(app);
+
+// ErrorHandlers middleware
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
